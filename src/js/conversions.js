e6930a903949181592b48252f002527e720d356b
Hemanth-20-2003 <hemux2000@gmail.com>
2026-03-22 12:00:39 +0530

    Coded UC9 for Arithmetic Operations

--- src/js/conversions.js
+++ src/js/conversions.js
@@ -2,6 +2,36 @@
 function convert(value, factor) {
   return value * factor;
 }
+
+/* =========================
+   UC-JS-09: Arithmetic Operation
+========================= */
+export function performArithmetic(v1, v2normalised, op) {
+  // ❌ Validate inputs
+  if (!Number.isFinite(v1) || !Number.isFinite(v2normalised)) {
+    throw new Error("Invalid numbers");
+  }
+
+  switch (op) {
+    case "+":
+      return parseFloat((v1 + v2normalised).toFixed(6));
+
+    case "-":
+      return parseFloat((v1 - v2normalised).toFixed(6));
+
+    case "*":
+      return parseFloat((v1 * v2normalised).toFixed(6));
+
+    case "/":
+      if (v2normalised === 0) {
+        throw new Error("Divide by zero");
+      }
+      return parseFloat((v1 / v2normalised).toFixed(6));
+
+    default:
+      throw new Error("Unknown operator");
+  }
+}
 /* =========================
    UC-JS-08: Compare Two Values
 ========================= */
